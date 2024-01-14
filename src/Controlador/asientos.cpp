118c25e21f861060c2f3e45b6963bf1f8b3b3de8
Kevin Alvarez <108185745+Kalvarez21g@users.noreply.github.com>
2024-01-14 01:44:53 +0100

    Implementación de los asientos

--- src/Controlador/asientos.cpp
+++ src/Controlador/asientos.cpp
@@ -4,7 +4,10 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QSpinBox>
+#include <QDialogButtonBox>
 #include <QRandomGenerator>
+#include <QtSql>
 
 asientos::asientos(QWidget *parent)
     : QDialog(parent)
@@ -19,33 +22,309 @@ asientos::~asientos()
 }
 
 int asientos::generarAsientos() {
-    return QRandomGenerator::global()->bounded(1, 125);
+    QSqlQuery asientosQuery;
+    QVector<int> asientosOcupados;
+
+    asientosQuery.prepare("SELECT numAsiento FROM asientos");
+    if (asientosQuery.exec()) {
+        while (asientosQuery.next()) {
+            asientosOcupados.append(asientosQuery.value("numAsiento").toInt());
+        }
+    }
+
+    int numAsiento;
+    do {
+        numAsiento = QRandomGenerator::global()->bounded(1, 125);
+    } while (asientosOcupados.contains(numAsiento));
+
+    return numAsiento;
 }
 
 void asientos::on_pushButton_4_clicked()
 {
-    int asiento = generarAsientos();
+    int precio = 50;
 
-    QLabel *labelZona = new QLabel("Usted ha seleccionado Tribuna");
-    QLabel *labelPrecio = new QLabel("El precio de las entradas es 20€");
-    QLabel *labelAsiento = new QLabel("Su Número de asiento es el: " + QString::number(asiento));
-    QPushButton *botonComprar = new QPushButton("Comprar");
+    QDialog dialogoEntradas(this);
 
-    QVBoxLayout *layout = new QVBoxLayout;
-    botonComprar->setStyleSheet("background-color:#01ff78; color:#fff; border-radius:10px;");
-    botonComprar->setFixedSize(85,30);
-    labelZona->setStyleSheet("border:1px solid #01ff78;");
-    labelPrecio->setStyleSheet("border:1px solid #01ff78;");
-    labelAsiento->setStyleSheet("border:1px solid #01ff78;");
-    layout->addWidget(labelZona);
-    layout->addWidget(labelPrecio);
-    layout->addWidget(labelAsiento);
-    layout->addWidget(botonComprar);
+    QLabel *labelCantidad = new QLabel("Número de Entradas: ");
+    QSpinBox *spinBoxEntradas = new QSpinBox();
+    spinBoxEntradas->setRange(1, 10);
+    labelCantidad->setBuddy(spinBoxEntradas);
 
-    QDialog *nuevaVentana = new QDialog(this);
-    nuevaVentana->setLayout(layout);
+    int asiento;
+    QVBoxLayout *layoutEntradas = new QVBoxLayout;
+    layoutEntradas->addWidget(labelCantidad);
+    layoutEntradas->addWidget(spinBoxEntradas);
 
-    nuevaVentana->exec();
+    QDialogButtonBox *botonesEntradas = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layoutEntradas->addWidget(botonesEntradas);
+
+    connect(botonesEntradas, &QDialogButtonBox::accepted, &dialogoEntradas, &QDialog::accept);
+    connect(botonesEntradas, &QDialogButtonBox::rejected, &dialogoEntradas, &QDialog::reject);
+
+    dialogoEntradas.setLayout(layoutEntradas);
+
+    if (dialogoEntradas.exec() == QDialog::Accepted) {
+        int cantidadEntradas = spinBoxEntradas->value();
+        int precioTotal = precio * cantidadEntradas;
+
+        QLabel *labelPrecioTotal = new QLabel("Precio total: " + QString::number(precioTotal) + "€");
+        QVBoxLayout *layoutAsientos = new QVBoxLayout;
+
+        QLabel *labelZona = new QLabel("Usted ha seleccionado Tribuna ");
+        layoutAsientos->addWidget(labelZona);
+        for (int i = 0; i < cantidadEntradas; ++i) {
+            asiento = generarAsientos();
+            QLabel *labelAsiento = new QLabel("Su Número de asiento es el: " + QString::number(asiento));
+            layoutAsientos->addWidget(labelAsiento);
+
+            QSqlQuery insertQuery;
+            insertQuery.prepare("INSERT INTO asientos (numAsiento, precio) VALUES (:numAsiento, :precio)");
+            insertQuery.bindValue(":numAsiento", asiento);
+            insertQuery.bindValue(":precio", precio);
+
+            if (insertQuery.exec()) {
+                qDebug() << "Asiento y precio guardados en la base de datos.";
+            } else {
+                qDebug() << "Error al guardar en la base de datos:" << insertQuery.lastError().text();
+            }
+        }
+
+        layoutAsientos->addWidget(labelPrecioTotal);
+
+        QPushButton *botonComprar = new QPushButton("Comprar");
+        botonComprar->setStyleSheet("background-color:#01ff78; color:#fff; border-radius:10px;");
+        botonComprar->setFixedSize(85,30);
+        layoutAsientos->addWidget(botonComprar);
+
+        QDialog *nuevaVentana = new QDialog(this);
+        nuevaVentana->setLayout(layoutAsientos);
+        connect(botonComprar, &QPushButton::clicked, nuevaVentana, &QDialog::accept);
+
+        if (nuevaVentana->exec() == QDialog::Accepted) {
+        } else {
+            qDebug() << "La ventana se cerró sin aceptar. No se guardará en la base de datos.";
+        }
+    } else {
+        qDebug() << "No se seleccionaron entradas.";
+    }
 }
 
+
+
+
+
+void asientos::on_pushButton_3_clicked()
+{
+    int precio = 40;
+    QDialog dialogoEntradas(this);
+
+    QLabel *labelCantidad = new QLabel("Número de Entradas: ");
+    QSpinBox *spinBoxEntradas = new QSpinBox();
+    spinBoxEntradas->setRange(1, 10);
+    labelCantidad->setBuddy(spinBoxEntradas);
+
+    int asiento;
+    QVBoxLayout *layoutEntradas = new QVBoxLayout;
+    layoutEntradas->addWidget(labelCantidad);
+    layoutEntradas->addWidget(spinBoxEntradas);
+
+    QDialogButtonBox *botonesEntradas = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layoutEntradas->addWidget(botonesEntradas);
+
+    connect(botonesEntradas, &QDialogButtonBox::accepted, &dialogoEntradas, &QDialog::accept);
+    connect(botonesEntradas, &QDialogButtonBox::rejected, &dialogoEntradas, &QDialog::reject);
+
+    dialogoEntradas.setLayout(layoutEntradas);
+
+    if (dialogoEntradas.exec() == QDialog::Accepted) {
+        int cantidadEntradas = spinBoxEntradas->value();
+        int precioTotal = precio * cantidadEntradas;
+
+        QLabel *labelPrecioTotal = new QLabel("Precio total: " + QString::number(precioTotal) + "€");
+        QVBoxLayout *layoutAsientos = new QVBoxLayout;
+
+        QLabel *labelZona = new QLabel("Usted ha seleccionado Preferente ");
+        layoutAsientos->addWidget(labelZona);
+        for (int i = 0; i < cantidadEntradas; ++i) {
+            asiento = generarAsientos();
+            QLabel *labelAsiento = new QLabel("Su Número de asiento es el: " + QString::number(asiento));
+            layoutAsientos->addWidget(labelAsiento);
+
+            QSqlQuery insertQuery;
+            insertQuery.prepare("INSERT INTO asientos (numAsiento, precio) VALUES (:numAsiento, :precio)");
+            insertQuery.bindValue(":numAsiento", asiento);
+            insertQuery.bindValue(":precio", precio);
+
+            if (insertQuery.exec()) {
+                qDebug() << "Asiento y precio guardados en la base de datos.";
+            } else {
+                qDebug() << "Error al guardar en la base de datos:" << insertQuery.lastError().text();
+            }
+        }
+
+        layoutAsientos->addWidget(labelPrecioTotal);
+
+        QPushButton *botonComprar = new QPushButton("Comprar");
+        botonComprar->setStyleSheet("background-color:#01ff78; color:#fff; border-radius:10px;");
+        botonComprar->setFixedSize(85,30);
+        layoutAsientos->addWidget(botonComprar);
+
+        QDialog *nuevaVentana = new QDialog(this);
+        nuevaVentana->setLayout(layoutAsientos);
+        connect(botonComprar, &QPushButton::clicked, nuevaVentana, &QDialog::accept);
+
+        if (nuevaVentana->exec() == QDialog::Accepted) {
+        } else {
+            qDebug() << "La ventana se cerró sin aceptar. No se guardará en la base de datos.";
+        }
+    } else {
+        qDebug() << "No se seleccionaron entradas.";
+    }
+}
+
+
+
+
+
+
+void asientos::on_pushButton_2_clicked()
+{
+    int precio = 25;
+
+    QDialog dialogoEntradas(this);
+
+    QLabel *labelCantidad = new QLabel("Número de Entradas: ");
+    QSpinBox *spinBoxEntradas = new QSpinBox();
+    spinBoxEntradas->setRange(1, 10);
+    labelCantidad->setBuddy(spinBoxEntradas);
+
+    int asiento;
+    QVBoxLayout *layoutEntradas = new QVBoxLayout;
+    layoutEntradas->addWidget(labelCantidad);
+    layoutEntradas->addWidget(spinBoxEntradas);
+
+    QDialogButtonBox *botonesEntradas = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layoutEntradas->addWidget(botonesEntradas);
+
+    connect(botonesEntradas, &QDialogButtonBox::accepted, &dialogoEntradas, &QDialog::accept);
+    connect(botonesEntradas, &QDialogButtonBox::rejected, &dialogoEntradas, &QDialog::reject);
+
+    dialogoEntradas.setLayout(layoutEntradas);
+
+    if (dialogoEntradas.exec() == QDialog::Accepted) {
+        int cantidadEntradas = spinBoxEntradas->value();
+        int precioTotal = precio * cantidadEntradas;
+
+        QLabel *labelPrecioTotal = new QLabel("Precio total: " + QString::number(precioTotal) + "€");
+        QVBoxLayout *layoutAsientos = new QVBoxLayout;
+
+        QLabel *labelZona = new QLabel("Usted ha seleccionado Fondo Norte ");
+        layoutAsientos->addWidget(labelZona);
+        for (int i = 0; i < cantidadEntradas; ++i) {
+            asiento = generarAsientos();
+            QLabel *labelAsiento = new QLabel("Su Número de asiento es el: " + QString::number(asiento));
+            layoutAsientos->addWidget(labelAsiento);
+
+            QSqlQuery insertQuery;
+            insertQuery.prepare("INSERT INTO asientos (numAsiento, precio) VALUES (:numAsiento, :precio)");
+            insertQuery.bindValue(":numAsiento", asiento);
+            insertQuery.bindValue(":precio", precio);
+
+            if (insertQuery.exec()) {
+                qDebug() << "Asiento y precio guardados en la base de datos.";
+            } else {
+                qDebug() << "Error al guardar en la base de datos:" << insertQuery.lastError().text();
+            }
+        }
+
+        layoutAsientos->addWidget(labelPrecioTotal);
+
+        QPushButton *botonComprar = new QPushButton("Comprar");
+        botonComprar->setStyleSheet("background-color:#01ff78; color:#fff; border-radius:10px;");
+        botonComprar->setFixedSize(85,30);
+        layoutAsientos->addWidget(botonComprar);
+
+        QDialog *nuevaVentana = new QDialog(this);
+        nuevaVentana->setLayout(layoutAsientos);
+        connect(botonComprar, &QPushButton::clicked, nuevaVentana, &QDialog::accept);
+
+        if (nuevaVentana->exec() == QDialog::Accepted) {
+        } else {
+            qDebug() << "La ventana se cerró sin aceptar. No se guardará en la base de datos.";
+        }
+    } else {
+        qDebug() << "No se seleccionaron entradas.";
+    }
+}
+
+
+void asientos::on_pushButton_clicked()
+{
+    int precio = 25;
+    QDialog dialogoEntradas(this);
+
+    QLabel *labelCantidad = new QLabel("Número de Entradas: ");
+    QSpinBox *spinBoxEntradas = new QSpinBox();
+    spinBoxEntradas->setRange(1, 10);
+    labelCantidad->setBuddy(spinBoxEntradas);
+
+    int asiento;
+    QVBoxLayout *layoutEntradas = new QVBoxLayout;
+    layoutEntradas->addWidget(labelCantidad);
+    layoutEntradas->addWidget(spinBoxEntradas);
+
+    QDialogButtonBox *botonesEntradas = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layoutEntradas->addWidget(botonesEntradas);
+
+    connect(botonesEntradas, &QDialogButtonBox::accepted, &dialogoEntradas, &QDialog::accept);
+    connect(botonesEntradas, &QDialogButtonBox::rejected, &dialogoEntradas, &QDialog::reject);
+
+    dialogoEntradas.setLayout(layoutEntradas);
+
+    if (dialogoEntradas.exec() == QDialog::Accepted) {
+        int cantidadEntradas = spinBoxEntradas->value();
+        int precioTotal = precio * cantidadEntradas;
+
+        QLabel *labelPrecioTotal = new QLabel("Precio total: " + QString::number(precioTotal) + "€");
+        QVBoxLayout *layoutAsientos = new QVBoxLayout;
+
+        QLabel *labelZona = new QLabel("Usted ha seleccionado Fondo Sur ");
+        layoutAsientos->addWidget(labelZona);
+        for (int i = 0; i < cantidadEntradas; ++i) {
+            asiento = generarAsientos();
+            QLabel *labelAsiento = new QLabel("Su Número de asiento es el: " + QString::number(asiento));
+            layoutAsientos->addWidget(labelAsiento);
+
+            QSqlQuery insertQuery;
+            insertQuery.prepare("INSERT INTO asientos (numAsiento, precio) VALUES (:numAsiento, :precio)");
+            insertQuery.bindValue(":numAsiento", asiento);
+            insertQuery.bindValue(":precio", precio);
+
+            if (insertQuery.exec()) {
+                qDebug() << "Asiento y precio guardados en la base de datos.";
+            } else {
+                qDebug() << "Error al guardar en la base de datos:" << insertQuery.lastError().text();
+            }
+        }
+
+        layoutAsientos->addWidget(labelPrecioTotal);
+
+        QPushButton *botonComprar = new QPushButton("Comprar");
+        botonComprar->setStyleSheet("background-color:#01ff78; color:#fff; border-radius:10px;");
+        botonComprar->setFixedSize(85,30);
+        layoutAsientos->addWidget(botonComprar);
+
+        QDialog *nuevaVentana = new QDialog(this);
+        nuevaVentana->setLayout(layoutAsientos);
+        connect(botonComprar, &QPushButton::clicked, nuevaVentana, &QDialog::accept);
+
+        if (nuevaVentana->exec() == QDialog::Accepted) {
+        } else {
+            qDebug() << "La ventana se cerró sin aceptar. No se guardará en la base de datos.";
+        }
+    } else {
+        qDebug() << "No se seleccionaron entradas.";
+    }
+}
 
--- src/Controlador/asientos.h
+++ src/Controlador/asientos.h
@@ -17,7 +17,14 @@ public:
 
 private slots:
     void on_pushButton_4_clicked();
+
     int generarAsientos();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
 
 private:
     Ui::asientos *ui;
